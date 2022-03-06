522dc01c2e993c399ce874f1a2810cf965ebad4a
Nico <limbaranico@yahoo.com>
2022-03-06 13:19:35 +0700

    add more visual feedback to project card link

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="project-card">
+  <a class="project-card" :href="link">
     <div class="project-card__title-wrapper">
       <h3 class="project-card__title">{{ title }} ({{ type }})</h3>
-      <a class="project-card__link" :href="link">
-        <font-awesome-icon
-          :icon="['fas', 'link']"
-          class="project-card__link-icon"
-        />
-      </a>
+      <font-awesome-icon
+        :icon="['fas', 'link']"
+        class="project-card__link-icon"
+      />
     </div>
     <p class="project-card__description">{{ description }}</p>
-  </div>
+  </a>
 </template>
 
 <script>
@@ -36,24 +34,29 @@ export default {
 
 <style lang="scss" scoped>
 .project-card {
-  @apply shadow;
+  @apply shadow-lg;
   @apply rounded;
   @apply inline-block;
   @apply w-full;
   @apply p-2;
   @apply m-2;
+  @apply select-none;
+
+  &:active {
+    @apply cursor-pointer;
+    @apply shadow;
+    @apply text-gray-900;
+  }
+
+  &:hover {
+    @apply cursor-pointer;
+    @apply shadow;
+    @apply text-gray-900;
+  }
 
   &__title-wrapper {
     @apply flex;
     @apply justify-between;
-  }
-
-  &__link {
-    @apply cursor-pointer;
-
-    &:hover {
-      @apply text-gray-800;
-    }
   }
 
   &__link-icon {
